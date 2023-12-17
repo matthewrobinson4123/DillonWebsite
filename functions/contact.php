964c9efbe8fe0ddaf9e3d2f39f0e90af8522b285
Matthew Robinson <matthew.robinson4123@gmail.com>
2023-12-16 19:19:40 -0500

    trying to fix email

--- functions/contact.php
+++ functions/contact.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST['email'])) {
 
-    $email_to = $_GET['emailTo'];
+    $email_to = "bearwarrior25@gmail.com";
     $email_subject = "Email From Portfolio Website";
 
     function problem($error)
